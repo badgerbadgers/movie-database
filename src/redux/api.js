e057d58b07f5070b675f9acf75d88dd5312c9a32
badgerbadgers <tim.almacen@gmail.com>
2022-10-13 20:06:38 -0700

    Co-authored-by: Monica <mnca16@users.noreply.github.com> Co-authored-by: MarissaLZ <MarissaLZ@users.noreply.github.com>

--- src/redux/api.js
+++ src/redux/api.js
@@ -3,4 +3,7 @@ import axios from "axios"
 const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 
 export const fetchMovies = async (movieName) =>
-  axios.get(`${API_ENDPOINT}&s=${movieName}`)
+  axios.get(`${API_ENDPOINT}&=${movieName}`)
+
+export const fetchMovie = async (movieId) =>
+  axios.get(`${API_ENDPOINT}&i=${movieId}`)
